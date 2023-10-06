2dd1984901da059c28b60b1d8e1645e5824c3c33
Thomas Lindeker <tlindeker0@gmail.com>
2023-10-06 19:55:28 +0200

    fetch data from http online api

--- app/src/main/java/fr/ernicani/learncode33/MainActivity.kt
+++ app/src/main/java/fr/ernicani/learncode33/MainActivity.kt
@@ -1,50 +1,66 @@
 package fr.ernicani.learncode33;
 
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import fr.ernicani.learncode33.R
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient;
+import okhttp3.Request
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var counterTextView: TextView
-    private lateinit var incrementButton: Button
-    private lateinit var decrementButton: Button
-
-    private var counter = 0
+    private lateinit var fetchDataButton: Button
+    private lateinit var fetchedDataTextView: TextView
+    private lateinit var cityEditText: EditText  // New EditText field
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize views
-        counterTextView = findViewById(R.id.counterTextView)
-        incrementButton = findViewById(R.id.incrementButton)
-        decrementButton = findViewById(R.id.decrementButton)
+        fetchDataButton = findViewById(R.id.fetchData)
+        fetchedDataTextView = findViewById(R.id.fetchedDataTextView)
+        cityEditText = findViewById(R.id.cityEditText)  // Initialize EditText
 
-        // Set initial counter value
-        updateCounterText()
-
-        // Set click listeners for buttons
-        incrementButton.setOnClickListener { incrementCounter() }
-        decrementButton.setOnClickListener { decrementCounter() }
+        fetchDataButton.setOnClickListener { fetchData() }
     }
 
-    private fun incrementCounter() {
-        counter++
-        updateCounterText()
-    }
+    private fun fetchData() {
+        val city = cityEditText.text.toString().trim()  // Get city name from EditText and trim whitespace
 
-    private fun decrementCounter() {
-        if (counter > 0) {
-            counter--
-            updateCounterText()
+
+        if (city.isNullOrEmpty()) {
+            fetchedDataTextView.text = "Please enter a city name"
+            return
         }
-    }
 
-    private fun updateCounterText() {
-        counterTextView.text = counter.toString()
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=66bb8eed95ce71a17230f561a7be99a5&units=metric")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            @SuppressLint("SetTextI18n")
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                this@MainActivity.runOnUiThread {
+                    fetchedDataTextView.text = "Une erreur est survenue"
+                }
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                if (response.isSuccessful) {
+                    val myResponse = response.body?.string()
+                    this@MainActivity.runOnUiThread {
+                        fetchedDataTextView.text = myResponse
+                    }
+                }
+            }
+        })
     }
 }
